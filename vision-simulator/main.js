04f51028431c7d95d7d08884534e19153cd0adfa
Dan Elder <daniel_elder@standardlife.com>
2019-05-15 22:54:14 +0100

    added basic menu for demo

---
+++ vision-simulator/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('click', function(e) {
+    var condition = e.target.getAttribute('data-condition');
+    if (condition) {
+        document.body.classList.add(condition);
+        document.body.innerHTML = '<a-scene embedded arjs="debugUIEnabled: false; sourceType: webcam;"><a-entity camera></a-entity></a-scene>';
+    }
+});
