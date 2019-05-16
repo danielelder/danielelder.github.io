21f0047ca11144598f4303a7294d6bc7c81a30ee
Dan Elder <daniel_elder@standardlife.com>
2019-05-16 08:10:01 +0100

    fixed ar

--- vision-simulator/main.js
+++ vision-simulator/main.js
@@ -2,6 +2,6 @@ document.addEventListener('click', function(e) {
     var condition = e.target.getAttribute('data-condition');
     if (condition) {
         document.body.classList.add(condition);
-        document.body.innerHTML = '<a-scene embedded arjs="debugUIEnabled: false; sourceType: webcam;"><a-entity camera></a-entity></a-scene>';
+        // document.body.innerHTML = '<a-scene embedded arjs="debugUIEnabled: false; sourceType: webcam;"><a-entity camera></a-entity></a-scene>';
     }
 });
